91c15d395edcb8982f7fa1be9df4893d4181ad73
Satria Utomo <62049293+SatriaUtomo@users.noreply.github.com>
2020-04-21 15:38:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,10 @@
     let x = 100;
     let y = 100;
 
+    //Aksi1
     ctx.save();
     ctx.beginPath();
-    ctx.translate(150, 200);
+    ctx.translate(300, 200);
     ctx.rect(0,0,150,100)
     ctx.fillStyle = 'red';
     ctx.fill();
@@ -20,85 +21,18 @@
 
     ctx.save();
     ctx.beginPath();
-    ctx.translate(150, 200);
-    ctx.rotate(Math.PI*-11/8);        
+    ctx.translate(300, 200);
+    ctx.rotate(Math.PI*16/8);        
     ctx.rect(0,0,150,25)
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(225, 250)
-    ctx.scale(1,1)
-    ctx.arc(0, 0, 35, 0, Math.PI*2);
-    ctx.fillStyle = 'orange';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(500, 200);
-    ctx.rect(0,0,150,100)
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(500, 200);
-    ctx.rotate(Math.PI*-9/8);        
-    ctx.rect(0,0,150,25)
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(575, 250)
-    ctx.scale(1,1)
-    ctx.arc(0, 0, 35, 0, Math.PI*2);
-    ctx.fillStyle = 'orange';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(150, 560);
-    ctx.rect(0,0,150,100)
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(150, 560);
-    ctx.rotate(Math.PI*-7/8);        
-    ctx.rect(0,0,150,25)
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(225, 610)
-    ctx.scale(1,1)
-    ctx.arc(0, 0, 35, 0, Math.PI*2);
-    ctx.fillStyle = 'orange';
+    ctx.fillStyle = 'black';
     ctx.fill();
     ctx.closePath();
     ctx.restore(); 
 
+    //Aksi2
     ctx.save();
     ctx.beginPath();
-    ctx.translate(540, 560);
+    ctx.translate(150, 500);
     ctx.rect(0,0,150,100)
     ctx.fillStyle = 'red';
     ctx.fill();
@@ -107,21 +41,31 @@
 
     ctx.save();
     ctx.beginPath();
-    ctx.translate(540, 540);
-    ctx.rotate(Math.PI*16/8);        
+    ctx.translate(150, 500);
+    ctx.rotate(Math.PI*-7/8);        
     ctx.rect(0,0,150,25)
-    ctx.fillStyle = 'blue';
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore();
+
+    //Aksi3
+    ctx.save();
+    ctx.beginPath();
+    ctx.translate(540, 500);
+    ctx.rect(0,0,150,100)
+    ctx.fillStyle = 'red';
     ctx.fill();
     ctx.closePath();
     ctx.restore();
 
     ctx.save();
     ctx.beginPath();
-    ctx.translate(615, 610)
-    ctx.scale(1,1)
-    ctx.arc(0, 0, 35, 0, Math.PI*2);
-    ctx.fillStyle = 'orange';
+    ctx.translate(540, 500);
+    ctx.rotate(Math.PI*-11/8);        
+    ctx.rect(0,0,150,25)
+    ctx.fillStyle = 'black';
     ctx.fill();
     ctx.closePath();
-    ctx.restore();  
+    ctx.restore();
 });
